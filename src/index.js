78d1b93c96bae16d63878ad584829f8a4e18f00b
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-17 18:21:09 +0000

    Geolocation app using Hooks.

--- src/index.js
+++ src/index.js
@@ -2,56 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Loader from './Loader';
 import SeasonDisplay from './SeasonDisplay';
+import useLocation from './useLocation';
 
-class App extends React.Component {
+const App = () => {
 
-  /*** Using a constructor to initialise state
-    constructor(props) {
-      super(props);
-      this.state = {
-        lat: null,
-        errorMessage: ''
-      };
-    } 
-  *******************************************/
+  const [lat, errorMessage] = useLocation();
 
-  // Usual way of initising state in React
-  state = {
-    lat: null,
-    errorMessage: ''
-  };
-
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // Call setState to update the state
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />
+  } else {
+    content = <Loader message="Please accept location request." />
   }
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
-    }
-    return <Loader message="Please accept location request." />
-  }
-
-  // React says we have to define render!
-  render() {
-    return (
-      <div>
-        {this.renderContent()}
-      </div>
-    )
-  }
-}
+  return (
+    <div className="border red">
+      {content}
+    </div>
+  )
+};
 
 ReactDOM.render(
   <App />,
